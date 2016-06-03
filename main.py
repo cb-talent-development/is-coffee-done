d254ed1a5077cc86d0db4c8062c9f54df70d4f73
toastercup <toastercup@gmail.com>
2016-06-03 02:51:56 +0000

    Initial functionality for coffee brewing status

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-#  done = False
-#
-#  infinite loop:
-#    avg = check_for_avg(10)
-#    if is_brewing?(avg):
-#      done = True
-#    elif is_done?(avg) and done
-#      SlackBot.send_notification("Coffee is ready to go!")
-#      done = False
-#
-#  def check_for_avg(number_of_iterations):
-#    number_of_iterations do:
-#      total += get_temperature
-#      sleep 1
-#    return total / number_of_iterations
-#
-#  def get_temperature:
-#    sensor.measure
-#
-#  def is_brewing?(temperature):
-#    return temperature > 200
-#
-#  def is_done?(temperature):
-#    return temperature < 190
+import random
+import time
+
+def average_temperature(time_range):
+  total = 0
+  for _ in range(time_range):
+    total = total + random.randrange(150, 250)
+    time.sleep(1)
+  average_temperature = total / time_range
+  return average_temperature
+
+def current_temperature():
+  return random.randrange(190, 205)
+
+def is_it_brewing(temperature):
+  return temperature > 200
+
+def is_it_done(temperature):
+  return temperature < 190
+
+if __name__ == "__main__":
+  done = False
+  while True:
+    temperature = average_temperature(10)
+    print temperature
+    if is_it_brewing(temperature):
+      print "it's brewing"
+      done = True
+    elif is_it_done(temperature) and done:
+      print "it's done"
+      # SlackBot.send_notification("Coffee is ready to go!")
+      print "Slack message! It's done!"
+      done = False
